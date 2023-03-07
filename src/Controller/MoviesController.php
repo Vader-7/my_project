ef9cdd735c7ca7e62d61feb8adf075cffeb070d5
タイラー <66812754+Vader-7@users.noreply.github.com>
2023-03-07 11:55:22 -0300

    update movie controller with repository methods

--- src/Controller/MoviesController.php
+++ src/Controller/MoviesController.php
@@ -2,18 +2,35 @@
 
 namespace App\Controller;
 
+use App\Entity\Movie;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MoviesController extends AbstractController
 {   
-    #[Route('/movies', name: 'app_movies')]
+    //constructor injection
+    private $em;
+    public function __construct(EntityManagerInterface $em)
+    {
+        $this->em = $em;
+    }
+
+    #[Route('/movies', name: 'movies')]
     public function index(): Response
     {
-        $movies = ["Avengers: Endgame", "The Dark Knight", "The Godfather", "The Shawshank Redemption", "The Lord of the Rings: The Return of the King", "Pulp Fiction", "Schindler's List", "Inception", "Fight Club", "The Matrix"];
-        return $this->render('movies/index.html.twig', array(
-            'movies' => $movies,
-        ));
+        // findAll() movies = SELECT * FROM movie
+        // find() movies = SELECT * FROM movie WHERE id = 5
+        // findBy() movies = SELECT * FROM movie ORDER BY id DESC
+        // findOneBy() movies = SELECT * FROM movie WHERE title = 'The Matrix' LIMIT 1
+        // count movies = SELECT COUNT(*) FROM movie WHERE id = 5
+        // getClassNames() = Shows the Entity class names
+        $repository = $this->em->getRepository(Movie::class);
+        $movies = $repository->findAll();
+
+        dd($movies);
+
+        return $this->render('index.html.twig');
     }    
 }
--- src/DataFixtures/MovieFixtures.php
+++ src/DataFixtures/MovieFixtures.php
@@ -14,7 +14,7 @@ class MovieFixtures extends Fixture
         $movie->setTitle('The Matrix');
         $movie->setReleaseYear(1999);
         $movie->setDescription('A computer hacker learns from mysterious rebels about the true nature of his reality and his role in the war against its controllers.');
-        $movie->setImagePath('https://image.tmdb.org/t/p/w500/6u1fYtxG5eqjhtCPDx04pJphQRW.jpg');
+        $movie->setImagePath('https://cdn.pixabay.com/photo/2015/11/28/10/52/binary-1066983__480.jpg');
         $movie-> addActor($this->getReference('actor'));
         $movie-> addActor($this->getReference('actor1'));
         $manager->persist($movie);
@@ -23,7 +23,7 @@ class MovieFixtures extends Fixture
         $movie1->setTitle('The Matrix Reloaded');
         $movie1->setReleaseYear(2003);
         $movie1->setDescription('Neo and the rebel leaders estimate that they have 72 hours until 250,000 probes discover Zion and destroy it and its inhabitants. During this, Neo must decide how he can save Trinity from a dark fate in his dreams.');
-        $movie1->setImagePath('https://image.tmdb.org/t/p/w500/3gIO6mCd4Q4PF1tuwcyI3sjFrtI.jpg');
+        $movie1->setImagePath('https://cdn.pixabay.com/photo/2015/11/28/10/52/binary-1066983__480.jpg');
         $movie1-> addActor($this->getReference('actor'));
         $movie1-> addActor($this->getReference('actor1'));
         $manager->persist($movie1);
