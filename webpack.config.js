a7b838e1def3b4a447237f269d5a5098d840b79e
タイラー <66812754+Vader-7@users.noreply.github.com>
2023-03-07 14:27:53 -0300

    added image support

--- webpack.config.js
+++ webpack.config.js
@@ -14,6 +14,11 @@ Encore
     // only needed for CDN's or subdirectory deploy
     //.setManifestKeyPrefix('build/')
     .enablePostCssLoader()
+    .copyFiles({
+        from: './assets/images',
+        to: 'images/[path][name].[hash:8].[ext]',
+        pattern: /\.(png|jpg|jpeg)$/
+    })
     /*
      * ENTRY CONFIG
      *
