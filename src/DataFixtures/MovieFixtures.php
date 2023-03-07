4d54e8b6419800c79a535d4fb6ffb8d19135b495
タイラー <66812754+Vader-7@users.noreply.github.com>
2023-03-07 11:28:41 -0300

    reference between tables added

--- src/DataFixtures/MovieFixtures.php
+++ src/DataFixtures/MovieFixtures.php
@@ -15,6 +15,8 @@ class MovieFixtures extends Fixture
         $movie->setReleaseYear(1999);
         $movie->setDescription('A computer hacker learns from mysterious rebels about the true nature of his reality and his role in the war against its controllers.');
         $movie->setImagePath('https://image.tmdb.org/t/p/w500/6u1fYtxG5eqjhtCPDx04pJphQRW.jpg');
+        $movie-> addActor($this->getReference('actor'));
+        $movie-> addActor($this->getReference('actor1'));
         $manager->persist($movie);
 
         $movie1 = new Movie();
@@ -22,9 +24,10 @@ class MovieFixtures extends Fixture
         $movie1->setReleaseYear(2003);
         $movie1->setDescription('Neo and the rebel leaders estimate that they have 72 hours until 250,000 probes discover Zion and destroy it and its inhabitants. During this, Neo must decide how he can save Trinity from a dark fate in his dreams.');
         $movie1->setImagePath('https://image.tmdb.org/t/p/w500/3gIO6mCd4Q4PF1tuwcyI3sjFrtI.jpg');
+        $movie1-> addActor($this->getReference('actor'));
+        $movie1-> addActor($this->getReference('actor1'));
         $manager->persist($movie1);
 
-        
         $manager->flush();
     }
 }
