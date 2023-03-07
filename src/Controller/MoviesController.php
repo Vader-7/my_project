527f7c729c4c1357cd7e0d3572133eb82847137a
タイラー <66812754+Vader-7@users.noreply.github.com>
2023-03-07 17:30:51 -0300

    fix: controller edit function

--- src/Controller/MoviesController.php
+++ src/Controller/MoviesController.php
@@ -11,20 +11,21 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Component\HttpFoundation\File\UploadedFile;
 
 class MoviesController extends AbstractController
 {
     //constructor injection
     private $em;
     private $movieRepository;
-    public function __construct(MovieRepository $movieRepository, EntityManagerInterface $em)
-    {
+    public function __construct(
+        MovieRepository $movieRepository,
+        EntityManagerInterface $em
+    ) {
         $this->movieRepository = $movieRepository;
         $this->em = $em;
     }
 
-    #[Route("/movies", methods: ['GET'], name: "movies")]
+    #[Route("/movies", methods: ["GET"], name: "movies")]
     public function index(): Response
     {
         // findAll() movies = SELECT * FROM movie
@@ -35,14 +36,14 @@ class MoviesController extends AbstractController
         // getClassNames() = Shows the Entity class names
         $movies = $this->movieRepository->findAll();
 
-        return $this->render("movies/index.html.twig", array(
-            "movies" => $movies
-        ));
+        return $this->render("movies/index.html.twig", [
+            "movies" => $movies,
+        ]);
     }
 
     //create a new movie
     #[Route("/movies/create", name: "create_movie")]
-    public function create(Request $request): Response 
+    public function create(Request $request): Response
     {
         $movie = new Movie();
         $form = $this->createForm(MovieFormType::class, $movie);
@@ -51,90 +52,96 @@ class MoviesController extends AbstractController
         if ($form->isSubmitted() && $form->isValid()) {
             $newMovie = $form->getData();
 
-            $imagePath = $form->get('imagePath')->getData();
+            $imagePath = $form->get("imagePath")->getData();
             if ($imagePath) {
-                $newFileName = uniqid() . '.' . $imagePath->guessExtension(); 
+                $newFileName = uniqid() . "." . $imagePath->guessExtension();
                 try {
                     $imagePath->move(
-                        $this->getParameter('kernel.project_dir') . '/public/uploads', 
+                        $this->getParameter("kernel.project_dir") .
+                            "/public/uploads",
                         $newFileName
                     );
                 } catch (FileException $e) {
                     return new Response($e->getMessage());
                 }
 
-                $newMovie -> setImagePath('/uploads/' . $newFileName);
+                $newMovie->setImagePath("/uploads/" . $newFileName);
             }
 
             $this->em->persist($newMovie);
             $this->em->flush();
 
-            return $this->redirectToRoute('movies');
+            return $this->redirectToRoute("movies");
         }
 
-        return $this->render("movies/create.html.twig", [ "form" => $form->createView()
+        return $this->render("movies/create.html.twig", [
+            "form" => $form->createView(),
         ]);
     }
 
     #[Route("/movies/edit/{id}", name: "edit_movie")]
     public function edit($id, Request $request): Response
     {
-        $movie = $this -> movieRepository -> find($id);
-        $form = $this -> createForm(MovieFormType::class, $movie);
-        $form -> handleRequest($request);
-        $imagePath = $form->get('imagePath')->getData();
+        $movie = $this->movieRepository->find($id);
+        $form = $this->createForm(MovieFormType::class, $movie);
+        $form->handleRequest($request);
+        $imagePath = $form->get("imagePath")->getData();
 
         if ($form->isSubmitted() && $form->isValid()) {
             if ($imagePath) {
-                if ($movie->getImagePath() !== null) {
-                    if (file_exists(
-                        $this->getParameter('kernel.project_dir') . $movie -> getImagePath()
-                        )){
-                            $this -> GetParameter('kernel.project_dir') . $movie -> getImagePath();
-
-                        $newFileName = uniqid() . '.' . $imagePath -> guessExtension();
-
-                        try {
-                            $imagePath->move(
-                                $this->getParameter('kernel.project_dir') . '/public/uploads', 
-                                $newFileName
-                            );
-                        } catch (FileException $e) {
-                            return new Response($e->getMessage());
-                        }
-
-                        $movie -> setImagePath('/uploads/' . $newFileName);
-                        $this -> em -> flush();
-
-                        return $this -> redirectToRoute('movies');
+                if ($movie->getImagePath() !== null || "") {
+                    if (
+                        file_exists(
+                            $this->getParameter("kernel.project_dir") .
+                                $movie->getImagePath()
+                        )
+                    ) {
+                        $this->getParameter("kernel.project_dir") .
+                            $movie->getImagePath();
                     }
+
+                    $newFName = uniqid() . "." . $imagePath->guessExtension();
+
+                    try {
+                        $imagePath->move(
+                            $this->getParameter("kernel.project_dir") .
+                                "/public/uploads",
+                            $newFName
+                        );
+                    } catch (FileException $e) {
+                        return new Response($e->getMessage());
+                    }
+
+                    $movie->setImagePath("/uploads/" . $newFName);
+                    $this->em->flush();
+
+                    return $this->redirectToRoute("movies");
                 }
-            }
-            else{
-                $movie -> setTitle($form -> get('title') -> getData()); 
-                $movie -> setReleaseYear($form -> get('releaseYear') -> getData());
-                $movie -> setDescription($form -> get('description') -> getData());
+            } else {
+                $movie->setTitle($form->get("title")->getData());
+                $movie->setReleaseYear($form->get("releaseYear")->getData());
+                $movie->setDescription($form->get("description")->getData());
 
                 $this->em->flush();
-                return $this->redirectToRoute('movies');
+                return $this->redirectToRoute("movies");
             }
 
             $this->em->persist($movie);
             $this->em->flush();
 
-            return $this->redirectToRoute('movies');
+            return $this->redirectToRoute("movies");
         }
 
-        return $this->render("movies/edit.html.twig", [ "form" => $form->createView()
+        return $this->render("movies/edit.html.twig", [
+            "form" => $form->createView(),
         ]);
     }
 
-    #[Route("/movies/{id}", methods: ['GET'], name: "movie")]
+    #[Route("/movies/{id}", methods: ["GET"], name: "movie")]
     public function show($id): Response
     {
         $movie = $this->movieRepository->find($id);
 
-        return $this->render("movies/show.html.twig", [ "movie" => $movie]
-        );
+        return $this->render("movies/show.html.twig", ["movie" => $movie]);
     }
 }
--- src/Form/MovieFormType.php
+++ src/Form/MovieFormType.php
@@ -13,45 +13,53 @@ use Symfony\Component\Form\Extension\Core\Type\FileType;
 
 class MovieFormType extends AbstractType
 {
-    public function buildForm(FormBuilderInterface $builder, array $options): void
-    {
+    public function buildForm(
+        FormBuilderInterface $builder,
+        array $options
+    ): void {
         $builder
-            ->add('title', TextType::class, [
-                'attr' => array(
-                    'class' => 'bg-transparent block border-b-2 w-full h-20 text-6xl outline-none',
-                    'placeholder' => 'Enter title...',
-                ),
-                'label' => false,
-                'required' => false
+            ->add("title", TextType::class, [
+                "attr" => [
+                    "class" =>
+                        "bg-transparent block border-b-2 w-full h-20 text-6xl outline-none",
+                    "placeholder" => "Enter title...",
+                ],
+                "label" => false,
+                "required" => false,
             ])
-            ->add('releaseYear', IntegerType::class, [
-                'attr' => array(
-                    'class' => 'bg-transparent block mt-10 border-b-2 w-full h-20 text-6xl outline-none',
-                    'placeholder' => 'Enter Release Year...'
-                ),
-                'label' => false,
-                'required' => false
+            ->add("releaseYear", IntegerType::class, [
+                "attr" => [
+                    "class" =>
+                        "bg-transparent block mt-10 border-b-2 w-full h-20 text-6xl outline-none",
+                    "placeholder" => "Enter Release Year...",
+                ],
+                "label" => false,
+                "required" => false,
             ])
-            ->add('description', TextareaType::class, [
-                'attr' => array(
-                    'class' => 'bg-transparent block mt-10 border-b-2 w-full h-60 text-6xl outline-none',
-                    'placeholder' => 'Enter Description...'
-                ),
-                'label' => false,
-                'required' => false,
+            ->add("description", TextareaType::class, [
+                "attr" => [
+                    "class" =>
+                        "bg-transparent block mt-10 border-b-2 w-full h-60 text-6xl outline-none",
+                    "placeholder" => "Enter Description...",
+                ],
+                "label" => false,
+                "required" => false,
             ])
-            ->add('imagePath', FileType::class, array(
-                'required' => false,
-                'mapped' => false   
-            ))
-            // ->add('actors')
-        ;
+            ->add(
+                "imagePath",
+                FileType::class,
+                [
+                    "required" => false,
+                    "mapped" => false,
+                ]
+                // ->add('actors')
+            );
     }
 
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
-            'data_class' => Movie::class,
+            "data_class" => Movie::class,
         ]);
     }
 }
