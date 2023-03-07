9ce54e7cdac15d2048fb7eb3569370d502a25c3d
タイラー <66812754+Vader-7@users.noreply.github.com>
2023-03-07 10:44:03 -0300

    movie controller added

--- src/Controller/MoviesController.php
+++ src/Controller/MoviesController.php
@@ -7,12 +7,13 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MoviesController extends AbstractController
-{
+{   
     #[Route('/movies', name: 'app_movies')]
     public function index(): Response
     {
-        return $this->render('movies/index.html.twig', [
-            'controller_name' => 'MoviesController',
-        ]);
-    }
+        $movies = ["Avengers: Endgame", "The Dark Knight", "The Godfather", "The Shawshank Redemption", "The Lord of the Rings: The Return of the King", "Pulp Fiction", "Schindler's List", "Inception", "Fight Club", "The Matrix"];
+        return $this->render('movies/index.html.twig', array(
+            'movies' => $movies,
+        ));
+    }    
 }
